29b93941bc3310a64afde7a6d0984d7473212e59
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-10-04 14:59:33 -0700

    Reference DOM elements and working button feature

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // import functions and grab DOM elements
 
+const userGuess = document.getElementById('user-guess');
+const guessResult = document.getElementById('guess-result');
+const gameResult = document.getElementById('game-result');
+
+const button = document.getElementById('button');
+
 // initialize global state
+let remainingTries = 4;
+let randomNum = Math.floor(Math.random()*20)+1;
 
 // set event listeners 
+button.addEventListener('click', ()=>{
+
+
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
