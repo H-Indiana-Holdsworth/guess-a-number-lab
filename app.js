30227fe5605d99f27bb1780de77597550b72ec61
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-10-04 16:19:32 -0700

    Adds guess button and displays user messages

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // import functions and grab DOM elements
 
 const userGuess = document.getElementById('user-guess');
-const guessResult = document.getElementById('guess-result');
-const gameResult = document.getElementById('game-result');
+const result = document.getElementById('result');
 
 const button = document.getElementById('button');
 
@@ -10,9 +9,32 @@ const button = document.getElementById('button');
 let remainingTries = 4;
 let randomNum = Math.floor(Math.random()*20)+1;
 
+
 // set event listeners 
 button.addEventListener('click', ()=>{
+  remainingTries--;
 
+  const userGuessValue = Number(userGuess.value);
+
+  if (userGuessValue === randomNum) {
+    result.textContent = 'You got it!'
+  }
+
+  else if (remainingTries.value === 0) {
+    result.textContent = "Mission failed, you'll get 'em next time"
+  } 
+  
+  else if (userGuessValue > randomNum) {
+    result.textContent = 'Guess lower!'
+  }
+
+  else if (userGuessValue < randomNum) {
+    result.textContent = 'Guess higher!'
+  }
+
+
+
+  console.log(randomNum);
 
 });
   // get user input
